9d6011af819b7f4a75064c84a007fd46fe2b5fe1
吴威亚 <2969666369@qq.com>
2024-07-22 23:11:48 +0800

    C

--- 求n的阶乘/求n的阶乘/求n的阶乘.c
+++ 求n的阶乘/求n的阶乘/求n的阶乘.c
@@ -142,29 +142,3 @@
 //}
 
 
-int Fib(int n)
-{
-	int a = 1;
-	int b = 1;
-	int c = 0;
-	
-	while (n >= 3)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-
-	return c;
-}
-
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	int ret = Fib(n);
-	printf("%d\n", ret);
-
-	return 0;
-}
